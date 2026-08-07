5dfae719267823567be1fc29644a2d5b580b3cdc
OrkanGoekcen <orkan.gok@gmail.com>
2026-08-07 13:58:10 +0200

    few first changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,54 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as fs from 'fs';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "bettervscharpoon" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('bettervscharpoon.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	const addCommand = vscode.commands.registerCommand('bettervscharpoon.add_to_harpoon_list', () => {
 		vscode.window.showInformationMessage('Hello World from BetterVSCHarpoon!');
+
+		const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+		if (workspaceFolder) {
+			const harpoonList = vscode.Uri.joinPath(
+				workspaceFolder.uri,
+				"better_harpoon_list.txt" // TODO: Change path
+			);
+
+			const activeEditor = vscode.window.activeTextEditor;
+			if (activeEditor == undefined) {
+				return;
+			}
+			let currentFilePath = activeEditor.document.fileName;
+
+			fs.writeFileSync(harpoonList.fsPath, `${currentFilePath}\n`, {
+				encoding: "utf8",
+				flag: "a+",
+			});
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	const jumpCommand = vscode.commands.registerCommand('bettervscharpoon.navigate_1', () => {
+
+		// TODO: Maybe single function?
+		const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+		if (workspaceFolder) {
+			const harpoonList = vscode.Uri.joinPath(
+				workspaceFolder.uri,
+				"better_harpoon_list.txt" // TODO: Change path
+			);
+
+			fs.readFile('./Index.html', 'utf8', (err, data) => {
+				if (err) throw err;
+
+				const lines = data.split('\n');
+				vscode.workspace.openTextDocument(vscode.Uri.file(lines[1]))
+			});
+
+			// Question: Wie bekomme ich x path aus der liste
+		}
+	});
+
+	context.subscriptions.push(addCommand, jumpCommand);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
