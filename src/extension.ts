8d1ed5e3c4ae00fbd77aeac758307cc4cf926ef2
OrkanGoekcen <orkan.gok@gmail.com>
2026-08-07 14:43:42 +0200

    added second navigation command

--- src/extension.ts
+++ src/extension.ts
@@ -27,8 +27,26 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	const jumpCommand = vscode.commands.registerCommand('bettervscharpoon.navigate_1', () => {
+	const jumpCommandOne = vscode.commands.registerCommand('bettervscharpoon.navigate_1', () => {
+		// TODO: Maybe single function?
+		const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+		if (workspaceFolder) {
+			const harpoonList = vscode.Uri.joinPath(
+				workspaceFolder.uri,
+				"better_harpoon_list.txt" // TODO: Change path
+			);
 
+			fs.readFile(harpoonList.fsPath, 'utf8', async(err, data) => {
+				if (err) throw err;
+
+				const lines = data.split('\n');
+				const document = await vscode.workspace.openTextDocument(lines[0]);
+				vscode.window.showTextDocument(document, vscode.ViewColumn.One, false);
+			});
+		}
+	});
+
+	const jumpCommandSecond = vscode.commands.registerCommand('bettervscharpoon.navigate_2', () => {
 		// TODO: Maybe single function?
 		const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
 		if (workspaceFolder) {
@@ -47,7 +65,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	context.subscriptions.push(addCommand, jumpCommand);
+	context.subscriptions.push(addCommand, jumpCommandOne, jumpCommandSecond);
 }
 
 export function deactivate() { }
